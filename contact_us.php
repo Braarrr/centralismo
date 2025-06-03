c80ead2019e8e3ff482a3b8acb76a25bf459f491
vrijeman <lutfiabrar02@gmail.com>
2025-06-03 20:29:29 +0700

    contact_us.php

--- contact_us.php
+++ contact_us.php
@@ -1,22 +1,12 @@
 <?php
 include 'koneksi.php';
 
-$nama = $_POST['nama']?? '';
-$email = $_POST['email']?? '';
-$pesan = $_POST['pesan']??'';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nama  = $_POST['nama'];
+    $email = $_POST['email'];
+    $pesan = $_POST['pesan'];
 
-if (!$nama || !$email || !$pesan) {
-    echo json_encode(['success' => false, 'message' => 'Semua field wajib diisi.']);
-    exit;
-}
-
-$stmt = $conn->prepare("INSERT INTO contact_us (nama, email, pesan) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $nama, $email, $pesan);
-
-
-if ($stmt->execute()) {
-    echo json_encode(['success' => true, 'message' => 'Pesan berhasil dikirim!']);
-} else {
-    echo json_encode(['success' => false, 'message' => 'Gagal menyimpan pesan.']);
+    mysqli_query($conn, "INSERT INTO kontak (nama, email, pesan) VALUES ('$nama', '$email', '$pesan')");
+    echo "<script>alert('Pesan berhasil dikirim!'); window.location.href='index.php';</script>";
 }
 ?>
